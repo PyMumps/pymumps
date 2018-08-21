cb0ea7719d97937bb4c43981835a5ac891532f48
Stephan Rave <stephanrave@uni-muenster.de>
2018-08-21 15:59:11 +0200

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md') as f:
 
 setup(
     name='PyMUMPS',
-    version='0.2',
+    version='0.3',
     description='Python bindings for MUMPS, a parallel sparse direct solver',
     long_description=long_description,
     long_description_content_type='text/markdown',
